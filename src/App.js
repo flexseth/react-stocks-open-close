4626ea65f7d18b9333f9998dac22a6e3fe44ff58
Seth Miller <seth@flexperception.com>
2021-01-06 21:11:33 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// API docs
+// https://polygon.io/docs/get_v1_open-close__stocksTicker___date__anchor
+// -------------------------
 import './App.css';
+import React, {useState} from 'react'
+import DisplayOpenClose from './DisplayOpenClose'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+attributes: {
+  let ticker = 'AAPL'
 }
 
+function App() {
+
+  const url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=sqfI8sNznOJ1HZHHps3UaMcoeguYwkZ2'
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => { 
+     console.log(data['results'][0]['o'])
+
+    //  ReactDOM.render(
+    //   <><li>Daily open was {open}</li> 
+    //   <li>Daily close was {close}</li></>)
+
+
+
+    }) // end fetch
+  }
+  const [ticker, setTicker] = useState('AAPL')
+ß
+  let UI = (
+    <>
+      <input placeholder={ticker} onChange={() => setTicker("JUICE")} name="ticker"></input>
+      <button onClick={handleClick}>Search Open/Close</button>
+      <DisplayOpenClose ticker={ticker}></DisplayOpenClose>
+    </>
+  )
+  return UI
+}
 export default App;
